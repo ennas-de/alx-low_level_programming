b19bc5bc054360129658ca7aa78b8999a958d185
Muhammed Abdulhakeem Ibiyemi <dev.abdulhakeem@gmail.com>
2022-06-30 18:09:04 -0700

    Add 0x00-hello_world Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#define WORD "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+
+int main(void)
+{
+	int len = strlen(HELLO);
+	
+	write(1, HELLO, len)
+	
+		return (1);
+}
