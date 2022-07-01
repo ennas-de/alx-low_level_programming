4de933b1df34e4af3975fab0ff7384a5601e0901
Muhammed Abdulhakeem Ibiyemi <dev.abdulhakeem@gmail.com>
2022-06-30 17:06:28 -0700

    Add alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ * */
+
+int main() {
+
+	char str[100];
+
+	strcpy(str, "Programming is like building a multilingual puzzle");
+
+	puts(str);
+
+	return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * */
+
+int main() {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: $c", a);
+	printf("Size of an int: $d", b);
+	printf("Size of a long int: $ld", c);
+	printf("Size of a long long int: $ ld", d);
+	printf("Size of a float: $f", e);
+
+	return 0;
+}
+
