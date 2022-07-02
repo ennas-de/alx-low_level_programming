6fca715a2e46178260eec662a02e2b1cf506c44d
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-02 05:35:05 +0100

    Add 0x01-varaibles

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 18; n < 28; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 19; m < 28; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (l = 20; l < 28; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 25 || m != 26)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
