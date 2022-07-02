5a1a5c1381155b37696a9ccb7a40dab1987e16e0
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-02 05:26:29 +0100

    Add 0x01-varaibles

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
-		if(num != 9)
+		if(num != 57)
 		{
 			putchar(',');
 			putchar(' ');
