029418d07deec7407f5d872801c3767fc48933be
Muhammed Abdulhakeem Ibiyemi <dev.abdulhakeem@gmail.com>
2022-07-01 18:19:13 -0700

    Add 0x01-variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
 /* more headers goes there */
 
-
-
 /* betty style doc for function main goes there */
 
 int main(void)
-
 {
-
 	int n;
 
 	srand(time(0));
