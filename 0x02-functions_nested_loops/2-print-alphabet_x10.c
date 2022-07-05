13221d2769d6ff8098421c965e52e2f6ad11d24d
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 00:20:23 +0100

    Add 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print-alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Void (Nothing to return)
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch * 10);
+	}
+
+	_putchar('\n');
+}
