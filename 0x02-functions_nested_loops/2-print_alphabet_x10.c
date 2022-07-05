d201a42d9fb20898f26865378d4187b7b5508035
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 00:43:27 +0100

    Add 2-print_alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		char ch;
 
-		for (ch = 97; i < 122; ch++)
+		for (ch = 97; ch < 122; ch++)
 			_putchar(ch);
-		_putchar('\n');
+		_putchar('\n')
 	}
 }
