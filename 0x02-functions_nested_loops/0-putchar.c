fbdbb326b2a22ce033a9eef32c2df6079a292ad5
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-05 22:39:47 +0100

    0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,18 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
+	_putchar('t');
 	_putchar('c');
-        _putchar('h');
+	_putchar('h');
 	_putchar('a');
-        _putchar('r');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
