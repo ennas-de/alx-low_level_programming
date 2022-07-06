92979e393f95e8e74c4ab92093737b4864c60f8b
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 02:05:52 +0100

    Add 5-sign.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		return (0 - c);
+	return (c - 0);
+}
