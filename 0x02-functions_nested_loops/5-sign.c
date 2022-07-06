bc827c99431cb60e2d553425b8b97f9cda6a9c55
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 01:57:43 +0100

    Add 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	else if (n < 0)
+	{
 		_putchar('-');
+	}
 	
 	return (-1);
 }
