9e3524ce46b81a6096e550fa13e7dd4cb871906a
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 02:32:03 +0100

    Add 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
+ * @c: the int to extract the last digit from
  * Return: value of the last digit
  */
 
