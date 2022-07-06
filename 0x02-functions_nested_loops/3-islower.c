adbf17061fb13b404d0b51153a580a02dd808c58
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 01:03:43 +0100

    Add 3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * islower - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	char ch;
+	for (ch = 97; ch <= 122; ch++)
+		if (c == ch)
+			return 1;
+       		else 
+			return 0;
+
+} 
