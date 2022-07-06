9d3d1652b5319035be354c5df4e3d9cb9299a4da
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-06 01:46:17 +0100

    Add 3-islower.c file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha: checks for alphabetic character
- *
- * @c: function input
- *
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 
