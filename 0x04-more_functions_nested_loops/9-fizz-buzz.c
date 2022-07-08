5b6eebf8fe3dcab2b2cdb14697ba1c0ddf572601
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-08 02:54:35 +0100

    Add 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz-buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -37,7 +37,7 @@ int main(void)
 
 		i++;
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 
 int _putchar(char c);
 int _isdigit(int c);
+int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
