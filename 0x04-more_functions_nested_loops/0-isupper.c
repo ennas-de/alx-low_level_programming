898f8b976d8dd7a31d4ced92612b32648b41deaf
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-07-08 01:57:03 +0100

    Add 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the letter is uppercase
+ * @c: interger to be checked
+ * Return: 1 if c is uppercase
+ */
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
