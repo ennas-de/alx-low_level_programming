48708222df1663b7988d0f30e7bb27b877b5f55f
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-08-17 01:43:42 +0100

    Add 101-print_listint_safe.c orrections

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,7 +17,7 @@ size_t print_listint_safe(const listint_t *head)
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
-			head = head -> next;
+			head = head->next;
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
