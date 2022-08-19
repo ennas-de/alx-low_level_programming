9c508bab673ab25950e149885697881b4853a8c5
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-08-19 03:57:17 +0100

    Add 2-set_bit.c file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 for success, -1 for failure.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
