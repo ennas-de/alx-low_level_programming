953ff4b90afe44eb15680c1d255e435e25d6ac14
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-09-30 04:38:51 +0100

    Add 5-get_nodeint

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_dnodeint_at_index - finds a specific node of a linked list
  * @head: pointer to the beginning of the list
- * @index: index of the node to retrieve
+ * @index: index of the node to retrieve.
  *
  * Return: pointer to the indexed node, or NULL on failure
  */
