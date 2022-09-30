fc10d609622ddec6f0715a68ce196c7b28725edb
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-09-30 04:39:30 +0100

    Add 6-sum_dlistint

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,7 +4,7 @@
  * sum_dlistint - sums all of the data of a dlistint_t linked list
  * @head: pointer to the beginning of the linked list
  *
- * Return: sum of all data, or 0 if the list is empty
+ * Return: sum of all data, or 0 if the list is empty.
  */
 int sum_dlistint(dlistint_t *head)
 {
