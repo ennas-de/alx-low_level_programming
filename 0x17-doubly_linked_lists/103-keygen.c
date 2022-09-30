5bef988a04842078bbf37694f31014bafd591277
Abdulhakeem Muhammed <ennas.de@gmail.com>
2022-09-30 04:42:56 +0100

    Add 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -7,7 +7,7 @@
  * @argc: number of arguments passed
  * @argv: arguments passed to main
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, 1 on error.
  */
 int main(int argc, char *argv[])
 {
